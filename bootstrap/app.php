15fee134888507f2e27570b15d8dc208693f23e0
hoainho <nhoxtvt@gmail.com>
2024-04-02 02:13:20 +0700

    remove middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,15 +9,15 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+    )->create();
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     $middleware->web(append: [
+    //         \App\Http\Middleware\HandleInertiaRequests::class,
+    //         \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+    //     ]);
 
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    //     //
+    // })
+    // ->withExceptions(function (Exceptions $exceptions) {
+    //     //
+    // })->create();
